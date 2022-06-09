b167fbc412acb1c99d67439508d69dbc9936a960
Sjors Lockhorst <lockhorstsjors@gmail.com>
2022-06-09 16:52:41 +0200

    Fixed test

--- test_latest_model.py
+++ test_latest_model.py
@@ -1,6 +1,6 @@
 import unittest
 
-from features import best_features
+from features import abscract_features_plus
 from build_models import train, training
 from evaluate_models import evaluate_model, test_sents
 
@@ -8,8 +8,8 @@ from evaluate_models import evaluate_model, test_sents
 class TestLatestModel(unittest.TestCase):
 
     def setUp(self):
-        self.PICKLE_TEST_FILE = "latest_greatest.pickle"
+        self.PICKLE_TEST_FILE = "abstract_features_plus.pickle"
 
     def test_accuracy(self):
-        train(self.PICKLE_TEST_FILE, best_features, training)
+        train(self.PICKLE_TEST_FILE, abscract_features_plus, training)
         print(evaluate_model(self.PICKLE_TEST_FILE, test_sents))
